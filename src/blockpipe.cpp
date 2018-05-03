886ae9dc81c24f9f68ade2919ecf66e4cd7dba81
Jiří Vozár <jiri.vozar@gmail.com>
2018-05-03 10:57:32 +0200

    Document BlockPipe class

--- src/blockpipe.cpp
+++ src/blockpipe.cpp
@@ -1,23 +1,23 @@
 #include "blockpipe.h"
 
-BlockPipe::BlockPipe(BlockScene *parent, BlockSlot *startSlot, BlockSlot *endSlot)
+BlockPipe::BlockPipe(BlockScene *parent, BlockSlot *outSlot, BlockSlot *inSlot)
 {
     this->parent_scene = parent;
     parent_scene->addItem(this);
 
-    this->startSlot = startSlot;
-    this->endSlot = endSlot;
-    startSlot->setPipe(this);
-    endSlot->setPipe(this);
+    this->outSlot = outSlot;
+    this->inSlot = inSlot;
+    outSlot->setPipe(this);
+    inSlot->setPipe(this);
 
-    setLine(QLineF(startSlot->scenePos(), endSlot->scenePos()));
+    setLine(QLineF(outSlot->scenePos(), inSlot->scenePos()));
     setPen(QPen(Qt::black, 2));
     setFlag(QGraphicsItem::ItemIsSelectable, true);
 }
 
 void BlockPipe::updatePosition()
 {
-    setLine(QLineF(startSlot->scenePos(), endSlot->scenePos()));
+    setLine(QLineF(outSlot->scenePos(), inSlot->scenePos()));
 }
 
 BlockPipe::~BlockPipe()
--- src/blockpipe.h
+++ src/blockpipe.h
@@ -9,18 +9,30 @@
 class BlockScene;
 class BlockSlot;
 
+/*!
+ * \brief Pipe class for connecting blocks.
+ */
 class BlockPipe : public QGraphicsLineItem
 {
 public:
-    BlockPipe(BlockScene *parent, BlockSlot *startSlot, BlockSlot *endSlot);
+    /*!
+     * \brief Pipe constructor.
+     * \param parent scene object the pipe is drawn on
+     * \param outSlot output slot of a block providing data
+     * \param inSlot input slot of a block for delivering data
+     */
+    BlockPipe(BlockScene *parent, BlockSlot *outSlot, BlockSlot *inSlot);
+
+    //! \brief Pipe destructor.
     ~BlockPipe();
 
+    //! \brief Update pipe position when moving attached block.
     void updatePosition();
 
 private:
-    BlockScene *parent_scene;
-    BlockSlot *startSlot;
-    BlockSlot *endSlot;
+    BlockScene *parent_scene;  //!< Parent scene object the pipe is drawn on.
+    BlockSlot *outSlot;        //!< Output slot of a block providing data
+    BlockSlot *inSlot;         //!< Input slot of a block for delivering data
 };
 
 #endif // BLOCKPIPE_H
